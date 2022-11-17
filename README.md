f2ae5d30e7769c05e8b40dd6d588944e2b0ffed9
Rob Sassack <rsassack25@gmail.com>
2022-11-17 02:14:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tenzies-scrimba
+
+A dice game built with React. This is a project from the [Scrimba React course](https://scrimba.com/learn/learnreact).
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Tenzies</p>
+    </div>
+  )
+}
+
+export default App
