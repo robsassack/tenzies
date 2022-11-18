ee797e19c5e19669b655b34a083595d6d2ae3241
Rob Sassack <rsassack25@gmail.com>
2022-11-18 00:52:28 -0500

    Created state for dice elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import React from "react";
 import Die from "./Die";
 import "./App.css";
 
 function App() {
-  let diceArr = [];
-  for (let i = 0; i < 10; i++) {
-    diceArr.push(<Die key={i} value={Math.floor(Math.random() * 6) + 1} />);
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    let diceArr = [];
+    for (let i = 0; i < 10; i++) {
+      diceArr.push(Math.ceil(Math.random() * 6));
+    }
+    return diceArr;
   }
+
+  const diceElements = dice.map((die, i) => {
+    return <Die key={i} value={die} />;
+  });
+
   return (
     <main>
-      <div className='dice-container'>{diceArr}</div>
+      <div className='dice-container'>{diceElements}</div>
     </main>
   );
 }
